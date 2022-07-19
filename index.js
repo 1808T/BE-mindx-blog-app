3bbc3ff29b0cfee077abad45bea99ee7356f6a17
1808T <nguyenanhtu18081998@gmail.com>
2022-07-19 19:00:43 +0700

    hot-fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname + "public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
 // ROUTE
 app.use("/api", authRouter);
